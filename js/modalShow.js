f7890efda89cbb5463450480c69a5a727891d377
Harry Marshall <harrymarshall@protonmail.com>
2022-07-08 14:23:42 +1000

    contuine on project page

--- js/modalShow.js
+++ js/modalShow.js
@@ -1,14 +1,27 @@
-var modal = document.getElementById(".bg-modal");
-
-
 const miningOpen = document.getElementById("mining-click");
 const cakeOpen = document.getElementById("cake-click");
 const webOpen = document.getElementById("web-click");
 
 miningOpen.addEventListener('click', function() {
-    document.querySelector('.bg-modal').style.display = 'flex';
+    document.querySelector('.mine-modal').style.display = 'flex';
 });
 
 document.querySelector('.close').addEventListener('click', function (){
-    document.querySelector('.bg-modal').style.display = 'none';
+    document.querySelector('.mine-modal').style.display = 'none';
+});
+
+cakeOpen.addEventListener('click', function() {
+    document.querySelector('.cake-modal').style.display = 'flex';
+});
+
+document.querySelector('.close-close').addEventListener('click', function (){
+    document.querySelector('.cake-modal').style.display = 'none';
+});
+
+webOpen.addEventListener('click', function() {
+    document.querySelector('.web-modal').style.display = 'flex';
+});
+
+document.querySelector('.close-close2').addEventListener('click', function (){
+    document.querySelector('.web-modal').style.display = 'none';
 });

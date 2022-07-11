60a52eedb978e006e4c24aa95b161867bbdd248b
Harry Marshall <harrymarshall@protonmail.com>
2022-07-11 15:32:03 +1000

    11/07/2022

--- js/modalShow.js
+++ js/modalShow.js
@@ -30,3 +30,25 @@ document.getElementById('close-3').addEventListener('click',
   function() {
     document.querySelector('.modal-3').style.display = 'none';
 });
+
+//4th Modal - API Crypto 
+document.getElementById('crypto-click').addEventListener('click',
+  function() {
+    document.querySelector('.modal-4').style.display = 'flex';
+});
+
+document.getElementById('close-4').addEventListener('click',
+  function() {
+    document.querySelector('.modal-4').style.display = 'none';
+});
+
+//5th Modal - Portfolio Website 
+document.getElementById('port-click').addEventListener('click',
+  function() {
+    document.querySelector('.modal-5').style.display = 'block';
+});
+
+document.getElementById('close-5').addEventListener('click',
+  function() {
+    document.querySelector('.modal-5').style.display = 'none';
+});

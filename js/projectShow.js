22480fec964e4d9bfaf1ff67c66277fb80fe3bc5
Harry Marshall <harrymarshall@protonmail.com>
2022-07-17 21:43:49 +1000

    working project display

--- js/projectShow.js
+++ js/projectShow.js
@@ -46,44 +46,39 @@ document.getElementById('web-click').addEventListener('click',
 
 });
 
-/*document.getElementById('back-button').addEventListener('click',
+document.getElementById('api-click').addEventListener('click',
   function() {
 
-    document.querySelector('.crypto-back').style.display = 'none';
-    document.querySelector('.crypto-front').style.display = 'block';
-    document.querySelector('.border-button').style.display = 'block';
-    document.querySelector('.column1').style.width = '33%';
-    document.querySelector('.project-column').style.flexDirection = 'row';
-    document.querySelector('.column3').style.display = 'block';
-    document.querySelector('.column2').style.display = 'block';
+    document.querySelector('.column5').style.display = 'none';
+    document.querySelector('.project-column2').style.display = 'block';
+    document.querySelector('.column4').style.width = '95%';
+    document.querySelector('.api-front').style.display = 'none';
 
-});*/
+    document.querySelector('.api-back').style.display = 'block';
 
-/*document.getElementById('back-button2').addEventListener('click',
+    document.querySelector('.more-btn').style.display = 'none';
+    document.querySelector('.return-projects').style.display = 'block';
+
+    document.querySelector('.project-column').style.display = 'none';
+
+});
+
+document.getElementById('port-click').addEventListener('click',
   function() {
 
-    document.querySelector('.sweet-back').style.display = 'none';
-    document.querySelector('.sweet-front').style.display = 'block';
-    document.querySelector('.border-button2').style.display = 'block';
-    document.querySelector('.column2').style.width = '33%';
-    document.querySelector('.project-column').style.flexDirection = 'row';
-    document.querySelector('.column1').style.display = 'block';
-    document.querySelector('.column3').style.display = 'block';
+    document.querySelector('.column4').style.display = 'none';
+    document.querySelector('.project-column2').style.display = 'block';
+    document.querySelector('.column5').style.width = '95%';
+    document.querySelector('.port-front').style.display = 'none';
 
-});*/
+    document.querySelector('.port-back').style.display = 'block';
 
-/*document.getElementById('back-button3').addEventListener('click',
-  function() {
+    document.querySelector('.more-btn').style.display = 'none';
+    document.querySelector('.return-projects').style.display = 'block';
 
-    document.querySelector('.personal-back').style.display = 'none';
-    document.querySelector('.personal-front').style.display = 'block';
-    document.querySelector('.border-button3').style.display = 'block';
-    document.querySelector('.column3').style.width = '33%';
-    document.querySelector('.project-column').style.flexDirection = 'row';
-    document.querySelector('.column1').style.display = 'block';
-    document.querySelector('.column2').style.display = 'block';
+    document.querySelector('.project-column').style.display = 'none';
 
-});*/
+});
 
 var toggleProjects = document.getElementById("more-btn");
 var toggleTarget = document.getElementById("project-column2");
@@ -101,13 +96,16 @@ function ToggleDisplayFunction(){
 }
 
 var toggleReturn = document.getElementById("return-btn");
-var toggleColumn1 = document.getElementById("column1");
-var toggleColumn2 = document.getElementById("column2");
+var toggleColumn1 = document.getElementById("crypto-back");
+var toggleColumn2 = document.getElementById("sweet-back");
+var toggleColumn3 = document.getElementById("personal-back");
+var toggleColumn4 = document.getElementById("api-back");
+var toggleColumn5 = document.getElementById("port-back");
 
 toggleReturn.addEventListener("click", CloseDisplayFunction);
 
 function CloseDisplayFunction () {
-    if(toggleColumn1.style.display == "flex"){
+    if(toggleColumn1.style.display == "block"){
 
         document.querySelector('.crypto-back').style.display = 'none';
         document.querySelector('.crypto-front').style.display = 'block';
@@ -119,7 +117,7 @@ function CloseDisplayFunction () {
         document.querySelector('.return-projects').style.display = 'none';
         document.querySelector('.more-btn').style.display = 'inline';
 
-    } else if (toggleColumn2.style.display == "flex") {
+    } else if (toggleColumn2.style.display == "block") {
 
         document.querySelector('.sweet-back').style.display = 'none';
         document.querySelector('.sweet-front').style.display = 'block';
@@ -131,7 +129,7 @@ function CloseDisplayFunction () {
         document.querySelector('.more-btn').style.display = 'inline';
         document.querySelector('.return-projects').style.display = 'none';
 
-    } else {
+    } else if (toggleColumn3.style.display == "block") {
 
         document.querySelector('.personal-back').style.display = 'none';
         document.querySelector('.personal-front').style.display = 'block';
@@ -143,5 +141,32 @@ function CloseDisplayFunction () {
         document.querySelector('.more-btn').style.display = 'inline';
         document.querySelector('.return-projects').style.display = 'none';
 
+    } else if (toggleColumn4.style.display == "block") {
+
+      document.querySelector('.project-column2').style.display = 'grid';
+      document.querySelector('.column5').style.display = 'block';
+      
+      document.querySelector('.api-front').style.display = 'block';
+      document.querySelector('.api-back').style.display = 'none';
+
+      document.querySelector('.more-btn').style.display = 'inline';
+      document.querySelector('.return-projects').style.display = 'none';
+
+      document.querySelector('.project-column').style.display = 'flex';
+
+    } else if (toggleColumn5.style.display == "block") {
+
+      document.querySelector('.project-column2').style.display = 'grid';
+      document.querySelector('.column4').style.display = 'block';
+      
+      document.querySelector('.port-front').style.display = 'block';
+      document.querySelector('.port-back').style.display = 'none';
+
+      document.querySelector('.more-btn').style.display = 'inline';
+      document.querySelector('.return-projects').style.display = 'none';
+
+      document.querySelector('.project-column').style.display = 'flex';
+
     }
+    
 }

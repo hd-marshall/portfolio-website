2468fba23a6bb2f4a9f1a3ca94b9035c1e6181a1
Harry Marshall <harrymarshall@protonmail.com>
2022-07-22 12:45:33 +1000

    22/07/2022

--- js/projectShowColumn.js
+++ js/projectShowColumn.js
@@ -3,11 +3,11 @@ var selectorProjectsValue = document.getElementById('project-selection').value;
 
 function getSelectedValue () {
     
-    if(selectorProjectsValue == 'crypto-project') {
+    if(selectorProjectsValue = 'crypto-project') {
         document.getElementById('column1').style.display = 'block';
         document.getElementById('column2').style.display = 'none';
         document.getElementById('column3').style.display = 'none';
-    } else if (selectorProjectsValue == 'sweet-project') {
+    } else if (selectorProjectsValue = 'sweet-project') {
         document.getElementById('column1').style.display = 'none';
         document.getElementById('column2').style.display = 'block';
         document.getElementById('column3').style.display = 'none';
